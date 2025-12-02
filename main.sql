736868d0247c8b8577dcafd9ae066a142248eff2
amine taleb <aminetaleb18@gmail.com>
2025-12-02 15:24:48 +0100

    TP1

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
-CREATE TABLE customer (
+/*CREATE TABLE customer (
     customer_id numeric(5,0),
     cust_name character varying(30),
     city character varying(15),
@@ -33,4 +33,30 @@ INSERT INTO salesman (salesman_id, name, city, commission) VALUES
 (5005, 'Pit Alex', 'London', 0.11),
 (5006, 'Mc Lyon', 'Paris', 0.14),
 (5007, 'Paul Adam', 'Rome', 0.13),
-(5003, 'Lauson Hen', 'San Jose', 0.12);
+(5003, 'Lauson Hen', 'San Jose', 0.12);*/
+
+/*CREATE TABLE orders (
+    ord_no INTEGER NOT NULL,
+    purch_amt REAL DEFAULT 0,
+    ord_date TEXT,
+    customer_id INTEGER NOT NULL,
+    salesman_id INTEGER NOT NULL
+);
+
+
+INSERT INTO orders (ord_no, purch_amt, ord_date, customer_id, salesman_id) VALUES
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.50, '2012-08-17', 3009, 5003),
+(70005, 2400.60, '2012-07-27', 3007, 5001),
+(70008, 5760.00, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.40, '2012-10-10', 3009, 5003),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.60, '2012-04-25', 3002, 5001),
+(70001, 150.50, '2012-10-05', 3005, 5002),
+(70007, 948.50, '2012-09-10', 3005, 5002),
+(70012, 250.45, '2012-06-27', 3008, 5002);*/
+
+/*INSERT INTO salesman (salesman_id, name, city, commission) VALUES
+(5017, 'OJ Simpsons', 'Florida', 0.15)*/
